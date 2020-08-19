7d76dd831a18a3e2fa9acaa04b322344bfd47d5a
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2020-08-19 19:20:53 +0300

    extracted from monorepo

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+This project contains infrastructure classes for obtaining data directly from javac's AST.
+These are used to collect references to java imports, classes, methods and fields (including compile-time constants) to facilitate
+JPS incremental compilation and building ".class"-based references index.
+The code is deliberately compiled using legacy java 6 and java 8 platforms to be able to be functional on all platforms starting from java 6.
+
+Originally sources were extracted from
+- community/jps/jps-builders-6
+- community/jps/java-ref-scanner-8
+
+The resulting jar artifact is not meant to be used directly, but rather as a library artifact in JetBrains IntelliJ platform code.
---
+++ javac-ast-extension/src/org/jetbrains/jps/javac/ast/api/JavacNameTable.java
@@ -0,0 +1,93 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.jps.javac.ast.api;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class JavacNameTable {
+  private final Elements myElements;
+  private Name myAsterisk;
+  private Name myInit;
+  private TypeElement myStreamElement;
+  private TypeElement myIteratorElement;
+  private TypeElement myIterableElement;
+  private TypeElement myStringElement;
+  private final Map<Name, String> myParsedNameCache = new LinkedHashMap<Name, String>() {
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Name, String> eldest) {
+      return size() > 2000;
+    }
+  };
+
+  public JavacNameTable(Elements elements) {
+    myElements = elements;
+  }
+
+  @NotNull
+  public String parseName(Name name) {
+    String value = myParsedNameCache.get(name);
+    if (value == null) {
+      myParsedNameCache.put(name, value = name.toString());
+    }
+    return value;
+  }
+
+  @NotNull
+  public String parseBinaryName(Element element) {
+    return parseName(myElements.getBinaryName((TypeElement)element));
+  }
+
+  public boolean isAsterisk(Name name) {
+    if (myAsterisk == null) {
+      myAsterisk = myElements.getName("*");
+    }
+    return myAsterisk == name;
+  }
+
+  public boolean isInit(Name name) {
+    if (myInit == null) {
+      myInit = myElements.getName("<init>");
+    }
+    return myInit == name;
+  }
+
+  @Nullable("if the type is not loaded to javac name table")
+  public TypeElement getStreamElement() {
+    if (myStreamElement == null) {
+      myStreamElement = myElements.getTypeElement("java.util.stream.Stream");
+    }
+    return myStreamElement;
+  }
+
+  @Nullable("if the type is not loaded to javac name table")
+  public TypeElement getIteratorElement() {
+    if (myIteratorElement == null) {
+      myIteratorElement = myElements.getTypeElement("java.util.Iterator");
+    }
+    return myIteratorElement;
+  }
+
+  @Nullable("if the type is not loaded to javac name table")
+  public TypeElement getIterableElement() {
+    if (myIterableElement == null) {
+      myIterableElement = myElements.getTypeElement("java.lang.Iterable");
+    }
+    return myIterableElement;
+  }
+
+  @NotNull
+  public TypeElement getStringElement() {
+    if (myStringElement == null) {
+      myStringElement = myElements.getTypeElement("java.lang.String");
+    }
+    return myStringElement;
+  }
+}
